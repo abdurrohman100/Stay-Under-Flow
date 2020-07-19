96943b239d7bcbdcd1de5a743c0567b0b8e1fd42
Mirzaq <mirzaq@sepitkom-info.com>
2020-07-19 13:12:28 +0700

    Add View Profil User

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,4 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {return view('welcome');});
 Route::get('/login', function () {return view('login');}) -> name('login');
 Route::get('/register', function () {return view('register');}) -> name('register');
+Route::get('/user', function () {return view('userprofile');});
